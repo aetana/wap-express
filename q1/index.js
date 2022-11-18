7630e34e76595125dd589185ce87f9d58e671e3e
aetana <amanuel.etanan@gmail.com>
2022-11-18 01:49:37 -0600

    express project version 1

--- q1/index.js
+++ q1/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
     let name = req.query.name;
+    let age = req.query.age;
     if (!name) {
         name = "person";
     }
-    res.send(`Welcome ${name}`);
+    if (!age) {
+        age = 0;
+    }
+    res.send(`Welcome ${name} and Age ${age}`);
 });
 app.listen(3000);
